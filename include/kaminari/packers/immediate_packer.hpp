84c20d797fa00fb21cdd07cc7585d5ad75ee4148
gpascualg <gpascualg93@gmail.com>
2020-09-18 16:33:56 +0200

    Missing packer in previous commit

--- include/kaminari/packers/immediate_packer.hpp
+++ include/kaminari/packers/immediate_packer.hpp
@@ -45,7 +45,8 @@ namespace kaminari
     void immediate_packer<Marshal, Allocator>::add(const packet::ptr& packet)
     {
         // Add to pending
-        auto pending = packer_t::_allocator.construct(packer_t::_allocator.allocate(1), packet);
+        auto pending = packer_t::_allocator.allocate(1);
+        std::allocator_traits<Allocator>::construct(packer_t::_allocator, pending, packet);
         packer_t::_pending.push_back(pending);
     }
 
